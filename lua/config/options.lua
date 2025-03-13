4d0c94971649539c76a65c0cd761665ab52e4f42
Jean-Francois Richard <jean-francois.richard@ubisoft.com>
2025-03-13 14:31:49 -0400

    feat: improving configuration (tabstop/relative/shell)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.relativenumber = false
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+if vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+end
